8918cd3692f0453de48ae38d4356b849314a5ef9
AnastasiaBozhok <anastasiia.bozhok@strataggem.com>
2020-10-28 11:17:32 +0100

    Estimate rotation angle modified
    First sort angles by value, then sort angles by line length
    + minor changes

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,12 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -24,14 +28,15 @@ import org.opencv.imgproc.Imgproc;
 
 public class Main {
 
-
-
 	private static final String outputFolder = "images/out/";
 
-	// 0 -- use all detected lines, 1 -- only those of the image size (probably none)
+	// 0 -- use all detected lines, 1 -- only those of the image size (probably
+	// none)
 	private static final double min_line_length_to_consider_percentage = 0.02;
-	private static final int close_kernel_size = 10;
-	private static final int dilate_lines_kernel_size = 4;
+	private static final int min_line_length_to_consider_pixels = 10;
+	private static final double close_kernel_size_as_percentage = 0.01;
+	private static final int min_close_kernel_size_pixels = 10;
+	private static final int dilate_lines_kernel_size_pixels = 4;
 
 	public static void main(String[] args) {
 		// For OpenCV (this is compulsory)
@@ -46,7 +51,16 @@ public class Main {
 		file.mkdir();
 
 		// read our original image
-		Mat image = Imgcodecs.imread("images/1.jpg");
+//		String fileName = "images/1.jpg";
+//		String fileName = "images/boxesOCR/barcodeExample.png";
+//		String fileName = "images/boxesOCR/5.jpg"; // problem
+		String fileName = "images/boxesOCR/2.jpg";
+		
+		Mat image = Imgcodecs.imread(fileName);
+		if (image.width() == 0) {
+			System.out.println("Problem reading the image from '" + fileName + "'. Exit.");
+			return;
+		}
 
 		// Straight it out! :)
 		Mat straightImage = straightenImage(image);
@@ -73,6 +87,10 @@ public class Main {
 		// "Connect" the letters and words
 		// initially only one Mat kernel =
 		// Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(20, 1));
+		int close_kernel_size = (int) Math
+				.round(close_kernel_size_as_percentage * Math.min(image.height(), image.width()));
+		close_kernel_size = Math.max(close_kernel_size, min_close_kernel_size_pixels);
+
 		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(close_kernel_size, 1));
 		Imgproc.morphologyEx(binary, binary, Imgproc.MORPH_CLOSE, kernel);
 		kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1, close_kernel_size));
@@ -85,7 +103,7 @@ public class Main {
 
 		// Dilate lines width for easier detection
 		kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE,
-				new Size(dilate_lines_kernel_size, dilate_lines_kernel_size));
+				new Size(dilate_lines_kernel_size_pixels, dilate_lines_kernel_size_pixels));
 		Imgproc.morphologyEx(binary, binary, Imgproc.MORPH_DILATE, kernel);
 
 		Imgcodecs.imwrite(outputFolder + "processedImage.jpg", binary);
@@ -106,6 +124,10 @@ public class Main {
 		Mat debugImage = binaryImage.clone();
 		Imgproc.cvtColor(debugImage, debugImage, Imgproc.COLOR_GRAY2BGR);
 
+		double min_line_length_to_consider = min_line_length_to_consider_percentage
+				* Math.min(binaryImage.width(), binaryImage.height());
+		min_line_length_to_consider = Math.max(min_line_length_to_consider, min_line_length_to_consider_pixels);
+
 		ArrayList<Double> angles = new ArrayList<Double>();
 		ArrayList<Double> distances = new ArrayList<Double>();
 		// Calculate the start and end point and the angle
@@ -121,16 +143,12 @@ public class Main {
 				Point end = new Point(x2, y2);
 
 				double distance = Math.sqrt(Math.pow(end.x - start.x, 2) + Math.pow(end.y - start.y, 2));
-				double min_line_length_to_consider = min_line_length_to_consider_percentage 
-						* Math.min(binaryImage.width(), binaryImage.height());
-
 				if (distance > min_line_length_to_consider) {
-					// Draw line on the "debug" image for visualization
+					// Draw line on the "debug" image for visualisation
 					Imgproc.line(debugImage, start, end, new Scalar(255, 255, 0), 5);
 
 					// Calculate the angle we need
 					angle = calculateAngleFromPoints(start, end);
-
 					if (angle < 0) {
 						angle = angle + 180;
 					}
@@ -143,7 +161,7 @@ public class Main {
 
 		Imgcodecs.imwrite(outputFolder + "detectedLines.jpg", debugImage);
 
-		angle = calculateAverageRotationUpToPi(angles, distances);
+		angle = estimateRotationAngleUpToPi(angles, distances);
 		return angle;
 	}
 
@@ -181,11 +199,7 @@ public class Main {
 		return rotateImage(rotatedImage, rotationAngle);
 	}
 
-	private static double calculateAverageRotationUpToPi(ArrayList<Double> angles, ArrayList<Double> distances) {
-
-//		Double[] dist_arr = toArray(distances);
-//		int[] sortedIndices = IntStream.range(0, dist_arr.length).boxed()
-//				.sorted((i, j) -> dist_arr[i].compareTo(dist_arr[j])).mapToInt(ele -> ele).toArray();
+	private static double estimateRotationAngleUpToPi(ArrayList<Double> angles, ArrayList<Double> distances) {
 
 		// Do not mind 90° rotation
 		if (!angles.isEmpty()) {
@@ -194,26 +208,48 @@ public class Main {
 			}
 		}
 		
-		// First we sort the angles
-		Collections.sort(angles);
-
-		//
-//		int max_angles_number_to_consider = 10;
-//		int angles_n = Math.min(sortedIndices.length, max_angles_number_to_consider);
-//		ArrayList<Double> angles_to_consider = new ArrayList<>(angles_n);
-//		for (int i = 0; i < angles_n - 1; i++) {
-//			angles_to_consider.add(angles.get(i));
-//		}
+		// First we sort the angles (keep indices to also sort distances)
+		Double[] anglesArray = toArray(angles);
+		int[] sortedIndicesAngles = IntStream.range(0, anglesArray.length).boxed()
+				.sorted((i, j) -> anglesArray[i].compareTo(anglesArray[j])).mapToInt(ele -> ele).toArray();
+		ArrayList<Double> anglesSortedByAngles = new ArrayList<>();
+		ArrayList<Double> distancesSortedByAngles = new ArrayList<>();		
+		for (int i = 0; i < angles.size(); i++) {
+			anglesSortedByAngles.add(angles.get(sortedIndicesAngles[i]));
+			distancesSortedByAngles.add(distances.get(sortedIndicesAngles[i]));
+		}
 
 		// Remove clearly aberrant angles
-		List<Double> outliers = getOutliersOfSortedList(angles);
-		angles.removeAll(outliers);
-		
-		// Double mean = calcualteAverage(angles_to_consider);
-		// TODO (Anastasia): get rid of outliers
-		Double median = getMedianOfSortedList(angles);
+		List<Integer> outliersIndices = getOutliersIndicesOfSortedList(anglesSortedByAngles);
+		for (int i = outliersIndices.size()-1; i >= 0; i--) {
+			anglesSortedByAngles.remove((int) outliersIndices.get(i));
+			distancesSortedByAngles.remove((int) outliersIndices.get(i));
+		}
 
-		Double angleToReturn = median;
+		// Sort distances (keep indices to also sort angles)
+		Double[] distArray = toArray(distancesSortedByAngles);
+		int[] sortedIndicesDistances = IntStream.range(0, distArray.length).boxed()
+				.sorted((i, j) -> distArray[i].compareTo(distArray[j])).mapToInt(ele -> ele).toArray();
+		ArrayList<Double> anglesSortedByDistance = new ArrayList<>();
+		ArrayList<Double> distancesSortedByDistance = new ArrayList<>();		
+		for (int i = sortedIndicesDistances.length - 1; i >= 0; i--) {
+			anglesSortedByDistance.add(anglesSortedByAngles.get(sortedIndicesDistances[i]));
+			distancesSortedByDistance.add(distancesSortedByAngles.get(sortedIndicesDistances[i]));
+		}
+
+		// Consider only max_angles_number_to_consider longest lines
+		int max_angles_number_to_consider = 20;
+		int angles_n = Math.min(anglesSortedByDistance.size(), max_angles_number_to_consider);
+		ArrayList<Double> angles_to_consider = new ArrayList<>(angles_n);
+		for (int i = 0; i < angles_n - 1; i++) {
+			angles_to_consider.add(anglesSortedByDistance.get(i));
+		}
+		
+		// TODO (Anastasia): get rid of outliers
+//		Double angleToReturn = getMedianOfSortedList(angles);
+		Double angleToReturn = getMedianOfSortedList(angles_to_consider);
+//		Double angleToReturn = calcualteAverage(angles_to_consider);
+
 		if (Math.abs(angleToReturn - 90) < angleToReturn)
 			angleToReturn = angleToReturn - 90;
 
@@ -227,7 +263,7 @@ public class Main {
 		return dist_arr;
 	}
 
-	private Double calcualteAverage(ArrayList<Double> angles) {
+	private static Double calcualteAverage(ArrayList<Double> angles) {
 		Double sum = 0d;
 		if (!angles.isEmpty()) {
 			for (Double angle : angles) {
@@ -272,33 +308,33 @@ public class Main {
 	}
 
 	// https://stackoverflow.com/questions/18805178/how-to-detect-outliers-in-an-arraylist
-    public static List<Double> getOutliersOfSortedList(List<Double> input) {
-        List<Double> output = new ArrayList<Double>();
-        List<Double> data1 = new ArrayList<Double>();
-        List<Double> data2 = new ArrayList<Double>();
-        if (input.size() % 2 == 0) {
-            data1 = input.subList(0, input.size() / 2);
-            data2 = input.subList(input.size() / 2, input.size());
-        } else {
-            data1 = input.subList(0, input.size() / 2);
-            data2 = input.subList(input.size() / 2 + 1, input.size());
-        }
-        double q1 = getMedianOfSortedList(data1);
-        double q3 = getMedianOfSortedList(data2);
-        double iqr = q3 - q1;
-        double lowerFence = q1 - 1.5 * iqr;
-        double upperFence = q3 + 1.5 * iqr;
-        for (int i = 0; i < input.size(); i++) {
-            if (input.get(i) < lowerFence || input.get(i) > upperFence)
-                output.add(input.get(i));
-        }
-        return output;
-    }
+	public static List<Integer> getOutliersIndicesOfSortedList(List<Double> input) {
+		List<Integer> outputIndices = new ArrayList<Integer>();
+		List<Double> data1 = new ArrayList<Double>();
+		List<Double> data2 = new ArrayList<Double>();
+		if (input.size() % 2 == 0) {
+			data1 = input.subList(0, input.size() / 2);
+			data2 = input.subList(input.size() / 2, input.size());
+		} else {
+			data1 = input.subList(0, input.size() / 2);
+			data2 = input.subList(input.size() / 2 + 1, input.size());
+		}
+		double q1 = getMedianOfSortedList(data1);
+		double q3 = getMedianOfSortedList(data2);
+		double iqr = q3 - q1;
+		double lowerFence = q1 - 1.5 * iqr;
+		double upperFence = q3 + 1.5 * iqr;
+		for (int i = 0; i < input.size(); i++) {
+			if (input.get(i) < lowerFence || input.get(i) > upperFence)
+				outputIndices.add(i);
+		}
+		return outputIndices;
+	}
 
-    private static double getMedianOfSortedList(List<Double> data) {
-        if (data.size() % 2 == 0)
-            return (data.get(data.size() / 2) + data.get(data.size() / 2 - 1)) / 2;
-        else
-            return data.get(data.size() / 2);
-    }
+	private static double getMedianOfSortedList(List<Double> data) {
+		if (data.size() % 2 == 0)
+			return (data.get(data.size() / 2) + data.get(data.size() / 2 - 1)) / 2;
+		else
+			return data.get(data.size() / 2);
+	}
 }
